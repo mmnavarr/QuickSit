028a9d437e1e247bfb1818c9b32da664b5413e3a
mmnavarr <malcolmnavarro@Malcolms-MacBook-Pro-2.local>
2015-02-12 11:19:28 -0500

    QuickSit Android Application
    Sheesh

---
+++ app/src/main/java/com/cyberplays/quicksit/MainActivity.java
@@ -0,0 +1,104 @@
+package com.cyberplays.quicksit;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView title, rangeTxt, pSizeTxt;
+    Button find;
+    EditText addr;
+    ImageButton currLocation;
+    SeekBar rangeBar, pSizeBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    //INITIALIZE THE VIEWS
+    private void initViews(){
+        title = (TextView) findViewById(R.id.title);
+
+        addr = (EditText) findViewById(R.id.address);
+
+        currLocation = (ImageButton) findViewById(R.id.currLocation);
+
+        rangeBar = (SeekBar) findViewById(R.id.range);
+        rangeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("view DEBUG", Integer.toString(progress));
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        rangeTxt = (TextView) findViewById(R.id.rangeTxt);
+
+        pSizeBar = (SeekBar) findViewById(R.id.pSize);
+        pSizeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        pSizeTxt = (TextView) findViewById(R.id.pSizeTxt);
+
+        find = (Button) findViewById(R.id.find);
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent();
+                Log.d("view DEBUG","FIND!");
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
