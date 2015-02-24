fc52f6770ae27cd455488b700cd95827970ba8f3
malcolmnavarro <mmnavarr@live.com>
2015-02-24 13:58:38 -0500

    Updated Listview

--- app/src/main/java/com/cyberplays/quicksit/ListActivity.java
+++ app/src/main/java/com/cyberplays/quicksit/ListActivity.java
@@ -1,39 +1,112 @@
 package com.cyberplays.quicksit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 
-public class ListActivity extends ActionBarActivity {
+public class ListActivity extends Activity {
+
+
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
+    private ListView mListView;
+
+    private ArrayList<Restaurant> array = new ArrayList<Restaurant>();
+    private MyAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+
+        if (savedInstanceState == null) {
+            setContentView(R.layout.activity_list);
+
+            if (isPlayServicesAvailable()) {
+                initMap();
+            }
+
+            initList();
+
+        }
     }
 
+    protected boolean isPlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (status == ConnectionResult.SUCCESS) {
+            Toast.makeText(getApplicationContext(), "Connection Success", Toast.LENGTH_SHORT).show();
+            return (true);
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+            Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "You're beat", Toast.LENGTH_SHORT).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        return (false);
+    }
+
+
+    private void initMap() {
+        map = ((MapFragment) this.getFragmentManager().findFragmentById(R.id.fragment)).getMap();
+
+        Marker hamburg = map.addMarker(new MarkerOptions()
+                .position(HAMBURG)
+                .title("Hamburg")
+                .snippet("Hamburg is cool too"));
+
+        Marker kiel = map.addMarker(new MarkerOptions()
+                .position(KIEL)
+                .title("Kiel")
+                .snippet("Kiel is cool"));
+
+        // Move the camera instantly to hamburg with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+    }
+
+    private void initList() {
+        mListView = (ListView) findViewById(R.id.list);
+
+        array.add(new Restaurant("Alto Cinco","Mexican", 1.6));
+        array.add(new Restaurant("Pastabilities", "Italian", 2.0));
+        array.add(new Restaurant("Dinosaur Bar-B-Que", "Southern BBQ", 2.3));
+        array.add(new Restaurant("The Mission Restaurant", "Mexican", 2.9));
+        array.add(new Restaurant("Francesca's Cucina", "American", 2.9));
+        array.add(new Restaurant("Tully's Good Times", "American", 3.0));
+        array.add(new Restaurant("Lemon Grass", "Thai", 3.3));
+        array.add(new Restaurant("Stella's Diner", "American", 3.7));
+
+        adapter = new MyAdapter(getApplicationContext(), R.layout.listview_item, array);
+
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //INTENT TO RESACTIVITY
+                return true;
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/cyberplays/quicksit/MyAdapter.java
@@ -0,0 +1,78 @@
+package com.cyberplays.quicksit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by malcolmnavarro on 2/3/15.
+ */
+public class MyAdapter extends ArrayAdapter<Restaurant> {
+    private final Context context;
+    private ArrayList<Restaurant> rests = new ArrayList<Restaurant>();
+    private final int resource;
+    private LayoutInflater mInflater;
+    private TextView res_name, res_type, res_dist;
+
+    public MyAdapter(Context context, int resource, ArrayList<Restaurant> rests) {
+        super(context, resource, rests);
+        this.context = context;
+        this.rests = rests;
+        this.resource = resource;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return rests.size();
+    }
+
+    @Override
+    public Restaurant getItem(int position) {
+        return rests.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return createViewFromResource(position, convertView, parent, resource);
+    }
+
+    private View createViewFromResource(int position, View convertView,
+                                        ViewGroup parent, int resource) {
+        View v;
+        if (convertView == null) {
+            v = mInflater.inflate(resource, parent, false);
+            res_name = (TextView) v.findViewById(R.id.list_item_name);
+
+            res_type = (TextView) v.findViewById(R.id.list_item_type);
+
+            res_dist = (TextView) v.findViewById(R.id.list_item_dist);
+        } else {
+            v = convertView;
+        }
+
+        res_name.setText(rests.get(position).getName());
+
+        res_type.setText(rests.get(position).getType());
+
+        res_dist.setText(Double.toString(rests.get(position).getDist()) + "mi");
+
+        return v;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return getCount();
+    }
+
+
+}
