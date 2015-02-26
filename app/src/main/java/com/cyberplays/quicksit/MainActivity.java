6b9b2903ac33603f54ea8205a57c13f4f085da54
hagan116 <sohagan@syr.edu>
2015-02-26 10:20:17 -0500

    update

--- app/src/main/java/com/cyberplays/quicksit/MainActivity.java
+++ app/src/main/java/com/cyberplays/quicksit/MainActivity.java
@@ -1,11 +1,13 @@
 package com.cyberplays.quicksit;
 
 import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.*;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +24,10 @@ public class MainActivity extends ActionBarActivity {
     ImageButton currLocation;
     Boolean lServices;
     SeekBar rangeBar, pSizeBar;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location lastKnownLocation, currentBestLocation;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,28 @@ public class MainActivity extends ActionBarActivity {
         addr = (EditText) findViewById(R.id.address);
 
         currLocation = (ImageButton) findViewById(R.id.currLocation);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                if (isBetterLocation(location,currentBestLocation)){
+                    currentBestLocation = location;
+                }
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
 
         rangeBar = (SeekBar) findViewById(R.id.range);
         rangeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -82,15 +110,66 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+
+
         find = (Button) findViewById(R.id.find);
         find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                locationManager.removeUpdates(locationListener);
                 Intent i = new Intent(getApplicationContext(), ListActivity.class);
                 startActivity(i);
                 Log.d("view DEBUG","FIND!");
             }
         });
     }
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
 
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
 }
