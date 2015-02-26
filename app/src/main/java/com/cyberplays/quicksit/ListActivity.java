7bbf90e50e9560d08d0ac2eb09dd9850200e6ec8
malcolmnavarro <mmnavarr@live.com>
2015-02-26 09:43:34 -0500

    update

--- app/src/main/java/com/cyberplays/quicksit/ListActivity.java
+++ app/src/main/java/com/cyberplays/quicksit/ListActivity.java
@@ -1,7 +1,9 @@
 package com.cyberplays.quicksit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -13,7 +15,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -21,14 +22,12 @@ import java.util.ArrayList;
 
 public class ListActivity extends Activity {
 
-
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
     private ListView mListView;
 
-    private ArrayList<Restaurant> array = new ArrayList<Restaurant>();
+    public ArrayList<Restaurant> array = new ArrayList<Restaurant>();
     private MyAdapter adapter;
+
 
 
     @Override
@@ -38,11 +37,11 @@ public class ListActivity extends Activity {
         if (savedInstanceState == null) {
             setContentView(R.layout.activity_list);
 
+            initList();
+
             if (isPlayServicesAvailable()) {
                 initMap();
             }
-
-            initList();
 
         }
     }
@@ -64,49 +63,55 @@ public class ListActivity extends Activity {
 
 
     private void initMap() {
-        map = ((MapFragment) this.getFragmentManager().findFragmentById(R.id.fragment)).getMap();
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment1)).getMap();
 
-        Marker hamburg = map.addMarker(new MarkerOptions()
-                .position(HAMBURG)
-                .title("Hamburg")
-                .snippet("Hamburg is cool too"));
+        //Loop through Restaurant array and add to map
+        for (int i = 0; i < array.size(); i++) {
+            Restaurant r = array.get(i);
 
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool"));
+            map.addMarker(new MarkerOptions()
+                .position(r.getLoc())
+                .title(r.getName())
+                .snippet(r.getType()));
+        }
 
         // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(array.get(1).getLoc(), 6));
 
         // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        map.animateCamera(CameraUpdateFactory.zoomTo(12), 2000, null);
     }
+
 
     private void initList() {
         mListView = (ListView) findViewById(R.id.list);
 
-        array.add(new Restaurant("Alto Cinco","Mexican", 1.6));
-        array.add(new Restaurant("Pastabilities", "Italian", 2.0));
-        array.add(new Restaurant("Dinosaur Bar-B-Que", "Southern BBQ", 2.3));
-        array.add(new Restaurant("The Mission Restaurant", "Mexican", 2.9));
-        array.add(new Restaurant("Francesca's Cucina", "American", 2.9));
-        array.add(new Restaurant("Tully's Good Times", "American", 3.0));
-        array.add(new Restaurant("Lemon Grass", "Thai", 3.3));
-        array.add(new Restaurant("Stella's Diner", "American", 3.7));
+        //Add Restaurants to array
+        array.add(new Restaurant("Alto Cinco","Mexican", 1.6, new LatLng(43.041165,-76.119486)));
+        array.add(new Restaurant("Pastabilities", "Italian", 2.0, new LatLng(43.04831,-76.155311)));
+        array.add(new Restaurant("Dinosaur Bar-B-Que", "Southern BBQ", 2.3, new LatLng(43.05249,-76.15467)));
+        array.add(new Restaurant("The Mission Restaurant", "Mexican", 2.9, new LatLng(43.04812,	-76.14742)));
+        array.add(new Restaurant("Francesca's Cucina", "American", 2.9, new LatLng(43.058505,-76.152333)));
+        array.add(new Restaurant("Tully's Good Times", "American", 3.0, new LatLng(43.056009,-76.088804)));
+        array.add(new Restaurant("Lemon Grass", "Thai", 3.3, new LatLng(43.047555,-76.154434)));
+        array.add(new Restaurant("Stella's Diner", "American", 3.7, new LatLng(43.06908,-76.165269)));
 
+        //Create list adapter with layout and array of restaurants to populate
         adapter = new MyAdapter(getApplicationContext(), R.layout.listview_item, array);
-
+        //Set list adapter
         mListView.setAdapter(adapter);
-
-        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        //Handle onclick to push all the information of click restaurant
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                //INTENT TO RESACTIVITY
-                return true;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), ResActivity.class);
+                i.putExtra("name", array.get(position).getName());
+                i.putExtra("type", array.get(position).getType());
+                i.putExtra("lat", array.get(position).getLoc().latitude);
+                i.putExtra("lng", array.get(position).getLoc().longitude);
+                startActivity(i);
+                Log.d("view DEBUG", "RESTAURANT CLICK!");
             }
         });
-
-
     }
 }
--- app/src/main/java/com/cyberplays/quicksit/ResActivity.java
+++ app/src/main/java/com/cyberplays/quicksit/ResActivity.java
@@ -1,17 +1,33 @@
 package com.cyberplays.quicksit;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v7.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 
 public class ResActivity extends ActionBarActivity {
 
-    TextView name, addr, phone;
+    private TextView name, addr, type;
+    private GoogleMap map;
+    private Intent i;
+    private double lat, lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,20 @@ public class ResActivity extends ActionBarActivity {
         if (savedInstanceState == null) {
             setContentView(R.layout.activity_res);
 
+            //SET ACTION BAR COLOR
+            ActionBar bar = getSupportActionBar();
+            bar.hide();
+            //bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#CE123E")));
+
+            //Get the Intent that started this activity
+            i = getIntent();
+
+            //if Internet -> setup map
+            if (isPlayServicesAvailable()) {
+                initMap();
+            }
+
+            //Init the views with the passed Restaurant data
             initViews();
         }
 
@@ -27,25 +57,89 @@ public class ResActivity extends ActionBarActivity {
 
     protected void initViews() {
         name = (TextView) findViewById(R.id.res_name);
+        name.setText(i.getStringExtra("name"));
 
         addr = (TextView) findViewById(R.id.res_addr);
+        addr.setText(getAddress());
 
-        phone = (TextView) findViewById(R.id.res_phone);
+        type = (TextView) findViewById(R.id.res_type);
+        type.setText(i.getStringExtra("type") + " Cuisine");
+
+    }
+
+
+    protected boolean isPlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (status == ConnectionResult.SUCCESS) {
+            Toast.makeText(getApplicationContext(), "Connection Success", Toast.LENGTH_SHORT).show();
+            return (true);
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+            Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "You're beat", Toast.LENGTH_SHORT).show();
+        }
+
+        return (false);
+    }
+
+    private void initMap() {
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment2)).getMap();
+
+        //Get latitude and logitude
+        lat = i.getDoubleExtra("lat", 43.041165);
+        lng = i.getDoubleExtra("lng", -76.119486);
+        LatLng loc = new LatLng(lat,lng);
+
+        //Add that specific restaurant to the map
+        map.addMarker(new MarkerOptions()
+                .position(loc)
+                .title(i.getStringExtra("name"))
+                .snippet(i.getStringExtra("type")));
+
+        // Move the camera instantly to hamburg with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 6));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+    }
+
+    //Used to convert LatLng to actual readable address
+    public String getAddress() {
+        try {
+            Geocoder geocoder;
+            List<Address> addresses;
+            geocoder = new Geocoder(getApplicationContext());
+            if (lat != 0 || lng != 0) {
+                addresses = geocoder.getFromLocation(lat,lng, 1);
+                String address = addresses.get(0).getAddressLine(0);
+                String city = addresses.get(0).getAddressLine(1);
+                String country = addresses.get(0).getAddressLine(2);
+                String fullAddr = (address + ", " + city);
+                return fullAddr;
+            } else {
+                Toast.makeText(getApplicationContext(), "latitude and longitude are null",Toast.LENGTH_LONG).show();
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 
     //MENU BUTTON ONCLICKLISTENER
-    private void menuClick(View v) {
+    public void menuClick(View v) {
         //Intent webIntent = Intent();
     }
 
     //YELP BNUTTON ONCLICKLISTENER
-    private void yelpClick(View v) {
+    public void yelpClick(View v) {
 
     }
 
     //MAKE RESERVATION BUTTON ONCLICKLISTENER
-    private void makeClick(View v) {
+    public void makeClick(View v) {
 
     }
 
